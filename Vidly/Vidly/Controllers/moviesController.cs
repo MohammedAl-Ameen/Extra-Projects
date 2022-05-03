94307b0b318374abd79f029d15825b9f0f07c144
MohammedAl-Amin <MohammedAl-Amin>
2022-05-03 20:33:32 +0300

    main

---
+++ Vidly/Vidly/Controllers/moviesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class moviesController : Controller
+    {
+        // GET: movies
+        public ActionResult Random()
+        {
+            var movie = new movie() { name = "Shrek!" };
+            
+            return View(movie);
+/*          return Content("hello world");
+            return HttpNotFound();
+            return new EmptyResult();
+            return redirectoaction("Index" , "Home" , new{page = 1, sortBy="name"});
+ */
+        }
+
+        public ActionResult edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+        public ActionResult index(int? pageindex , string sortby)
+        {
+            if (!pageindex.HasValue)
+            {
+                pageindex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortby))
+            {
+                sortby = "name";
+            }
+
+
+            return Content(string.Format("pageIndex={0}&sortby={1}", pageindex , sortby));
+        }
+    }
+}
